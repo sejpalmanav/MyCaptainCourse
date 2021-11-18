224b1e33403cd206cf2ab3daa5fa1fac8cfe5e66
Manav Sejpal <94636077+sejpalmanav@users.noreply.github.com>
2021-11-18 23:14:03 +0530

    Create product.cpp
    This is my second project for c++ course at MyCaptain.

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+ int main()
+ {
+     float a,b;
+     int product;
+     cout<<"Enter Any Two Numbers: "<<endl;
+     cin>>a>>b;
+     product=a*b;
+     cout<<"The Product of Two Numbers is: "<<product <<endl;
+     return 0;
+ }
